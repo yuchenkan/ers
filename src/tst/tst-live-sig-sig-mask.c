0b520ada9f24211837590a6dd1db097ed09cbcb9
yuchenkan <yuchenkan@gmail.com>
2019-03-01 13:15:06 +0800

    add tst/tst-live-sig-sig-mask

---
+++ src/tst/tst-live-sig-sig-mask.c
@@ -0,0 +1,37 @@
+#include <compiler.h>
+#include <common.h>
+
+#include <lib/tst-util.h>
+#include <tst/tst-util.h>
+#include <tst/tst-syscall.h>
+
+static aligned16 uint8_t stack[1024 * 1024];
+static struct tst_sys_clone_raise_args raise_args;
+
+static void
+sig_handler (int32_t sig) { eri_debug ("\n"); }
+
+noreturn void tst_live_start (void);
+
+noreturn void
+tst_live_start (void)
+{
+  struct tst_rand rand;
+  tst_rand_init (&rand);
+
+  struct eri_sigaction act = { 
+    sig_handler, ERI_SA_RESTORER, tst_assert_sys_sigreturn
+  };
+  tst_assert_sys_sigaction (ERI_SIGINT, &act, 0);
+
+  struct eri_sigset mask;
+  eri_sig_fill_set (&mask);
+
+  tst_sys_clone_raise_init_args (&raise_args, 0, stack, 0, 0);
+  tst_assert_sys_clone_raise (&raise_args);
+
+  tst_yield (tst_rand (&rand, 0, 8));
+  tst_assert_sys_sigprocmask (&mask, 0);
+
+  tst_assert_sys_exit_group (0);
+}
