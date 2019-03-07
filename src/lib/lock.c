7cd3e3644936d2e6e09d411e9f96e53046752bfd
yuchenkan <yuchenkan@gmail.com>
2019-03-08 00:09:45 +0800

    revert lock.S

---
+++ src/lib/lock.c
@@ -0,0 +1,22 @@
+#include <lib/syscall.h>
+#include <lib/atomic.h>
+
+void
+eri_assert_lock (int32_t *lock)
+{
+  while (eri_atomic_exchange (lock, 1))
+    {
+      uint64_t res = eri_syscall (futex, lock, ERI_FUTEX_WAIT, 1, 0);
+      eri_assert (! eri_syscall_is_error (res)
+		  || res == ERI_EAGAIN || res == ERI_EINTR);
+    }
+  eri_barrier ();
+}
+
+void
+eri_assert_unlock (int32_t *lock)
+{
+  eri_barrier ();
+  eri_atomic_store (lock, 0);
+  eri_assert_syscall (futex, lock, ERI_FUTEX_WAKE, 1);
+}
--- src/lib/tst/link.g
+++ src/lib/tst/link.g
@@ -1,6 +1,6 @@
 'use strict'; /* vim: set ft=javascript: */
 
 const rand = 'lib/tst/tst/tst-rand.c.o';
-const srcs = [ `${goal}.c.o`, rand ].concat ([ 'util.c', 'lock.S', 'buf.c', 'malloc.c', 'printf.c' ].map (s => `lib/tst/lib/${s}.o`));
+const srcs = [ `${goal}.c.o`, rand ].concat ([ 'util', 'lock', 'buf', 'malloc', 'printf' ].map (s => `lib/tst/lib/${s}.c.o`));
 
 await this.invoke ('goal/link.g', { srcs, ldflags: () => '' });
