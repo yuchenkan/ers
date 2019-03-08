e62217981cfcd82191ad97fd2d19a0601a731e19
yuchenkan <yuchenkan@gmail.com>
2019-03-08 12:21:28 +0800

    optimize lock, no futex wake if no waiter

--- src/lib/lock.c
+++ src/lib/lock.c
@@ -5,13 +5,23 @@
 void
 eri_assert_lock (struct eri_lock *lock)
 {
-  while (eri_atomic_exchange (&lock->lock, 1))
+  if (! eri_atomic_exchange (&lock->lock, 1))
+    {
+      eri_barrier ();
+      return;
+    }
+
+  eri_atomic_inc (&lock->wait);
+  eri_barrier ();
+  do
     {
       uint64_t res = eri_syscall (futex, &lock->lock, ERI_FUTEX_WAIT, 1, 0);
       eri_assert (! eri_syscall_is_error (res)
 		  || res == ERI_EAGAIN || res == ERI_EINTR);
     }
+  while (eri_atomic_exchange (&lock->lock, 1));
   eri_barrier ();
+  eri_atomic_dec (&lock->wait);
 }
 
 void
@@ -19,5 +29,6 @@ eri_assert_unlock (struct eri_lock *lock)
 {
   eri_barrier ();
   eri_atomic_store (&lock->lock, 0);
-  eri_assert_syscall (futex, &lock->lock, ERI_FUTEX_WAKE, 1);
+  if (eri_atomic_load_acq (&lock->wait))
+    eri_assert_syscall (futex, &lock->lock, ERI_FUTEX_WAKE, 1);
 }
