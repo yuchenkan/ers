7753f02f9e789baa9f5fe4c901dc3dcf8ef421fd
yuchenkan <yuchenkan@gmail.com>
2019-04-03 13:30:19 +0800

    add clone to tst-single-step

--- src/live/tst/tst-single-step.c
+++ src/live/tst/tst-single-step.c
@@ -6,15 +6,24 @@
 #include <tst/tst-atomic.h>
 #include <tst/tst-syscall.h>
 #include <live/tst/tst-util.h>
+#include <live/tst/tst-syscall.h>
 
+static int32_t tid;
 static uint64_t steps;
+static uint64_t raise_steps;
 static char *src = "ab";
+
+static uint8_t stack[1024 * 1024];
 
 static void
 sig_handler (int32_t sig, struct eri_siginfo *info, struct eri_ucontext *ctx)
 {
   eri_debug ("single step: %lx\n", ctx->mctx.rip);
-  ++steps;
+
+  if (tst_assert_syscall (gettid) != tid)
+    tst_atomic_inc (&raise_steps);
+
+  tst_atomic_inc (&steps);
   eri_assert (sig == ERI_SIGTRAP);
   eri_assert (eri_si_single_step (info));
 }
@@ -25,11 +34,16 @@ eri_noreturn void
 tst_live_start (void)
 {
   eri_info ("start\n");
+  tid = tst_assert_syscall (gettid);
+
   struct eri_sigaction act = {
     sig_handler, ERI_SA_SIGINFO | ERI_SA_RESTORER, tst_assert_sys_sigreturn
   };
   eri_sig_fill_set (&act.mask);
   tst_assert_sys_sigaction (ERI_SIGTRAP, &act, 0);
+
+  struct tst_sys_clone_raise_args args;
+  tst_sys_clone_raise_init_args (&args, 0, stack, 0, 1);
 
   tst_enable_trace ();
 
@@ -42,6 +56,11 @@ tst_live_start (void)
   register uint32_t c asm ("ecx") = 2;
   asm (ERI_STR (ERS_SYNC_ASYNC (1, rep movsb))
        : : "r" (c), "r" (d), "r" (s) : "memory");
+
+  tst_assert_sys_clone_raise (&args);
+  tst_assert_sys_futex_wait (&args.alive, 1, 0);
+
+  eri_assert (tst_atomic_load (&raise_steps));
 
   tst_assert_sys_exit (0);
 }
