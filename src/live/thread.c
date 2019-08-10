c7bc2f906802d69f9c6525e332069c0519ba9a51
yuchenkan <yuchenkan@gmail.com>
2019-08-10 12:43:37 +0800

    Revert "fix sigtimedwait"
    This reverts commit 1c30cb742568d0688a1684d7ec56949ccc7a5701.

--- src/live/thread.c
+++ src/live/thread.c
@@ -1531,10 +1531,8 @@ DEFINE_SYSCALL (rt_sigtimedwait)
 
   if (! eri_entry__sig_wait_pending (entry, user_timeout ? &timeout : 0))
     {
-      /* XXX: optimize mask all & reset */
-      if (eri_live_signal_thread__sig_mask_all (sig_th))
+      if (eri_live_signal_thread__sig_tmp_mask_async (sig_th, mask))
 	{
-	  eri_live_signal_thread__sig_reset (sig_th, 0);
 	  eri_atomic_store (&th->sig_force_deliver, 0, 1);
 	  rec.res.result = ERI_EAGAIN;
 	  goto record;
